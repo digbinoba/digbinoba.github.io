2ee9d159475c5e17e15e151b4c3dc0180753057d
delanoigbinoba <digbinoba@ufl.edu>
2023-04-25 09:48:48 -0400

    changed order of projects

--- src/assets/files/projects.js
+++ src/assets/files/projects.js
@@ -9,12 +9,24 @@ export const projects = [
     liveDemoLink: "https://deft-bombolone-6768a1.netlify.app/",
   },
   {
-    title: "Hi Score",
+    title: "ChefMate",
+    projectType: "UX/UI",
+    thumbnail: "/project-img/p3cover.png",
+    tags: ["Figma"],
+    details:
+      "ChefMate is an online platform where users can find recipes and cooking techniques from a wide library of user uploaded videos and content.",
+    caseStudyLink:
+      "https://www.behance.net/gallery/163329633/ChefMate-Cooking-Learning-Platform-UX-Case-Study",
+    liveDemoLink:
+      "https://www.figma.com/proto/jchu5msJk4DOXW1mUDjfHg/P3-Prototype?node-id=1%253A213&scaling=min-zoom&page-id=0%253A1&starting-point-node-id=1%253A213&show-proto-sidebar=1",
+  },
+  {
+    title: "Level Up",
     projectType: "UX/UI",
     thumbnail: "/project-img/hi-score-thumb.png",
     tags: ["Figma", "React Native", "Firebase"],
     details:
-      "Hi Score is a mobile app that allows users to keep track of their scores, rankings, and leaderboards. ",
+      "Level Up is a mobile app that allows users to keep track of their scores, rankings, and leaderboards. ",
     caseStudyLink:
       "https://www.behance.net/gallery/162767945/Level-Up-Hi-Score-Tracking-App-UX-Case-Study",
     liveDemoLink: "https://github.com/digbinoba/level-up",
@@ -30,18 +42,6 @@ export const projects = [
       "https://www.behance.net/gallery/161742679/Pandoras-Box-Food-Truck-Design-UXUI-Case-Study",
     liveDemoLink:
       "https://xd.adobe.com/view/997e1911-a5ed-4c4f-b66a-1a288069eb8e-cc2d/",
-  },
-  {
-    title: "ChefMate",
-    projectType: "UX/UI",
-    thumbnail: "/project-img/p3cover.png",
-    tags: ["Figma"],
-    details:
-      "ChefMate is an online platform where users can find recipes and cooking techniques from a wide library of user uploaded videos and content.",
-    caseStudyLink:
-      "https://www.behance.net/gallery/163329633/ChefMate-Cooking-Learning-Platform-UX-Case-Study",
-    liveDemoLink:
-      "https://www.figma.com/proto/jchu5msJk4DOXW1mUDjfHg/P3-Prototype?node-id=1%253A213&scaling=min-zoom&page-id=0%253A1&starting-point-node-id=1%253A213&show-proto-sidebar=1",
   },
   {
     title: "AR Shooter",
